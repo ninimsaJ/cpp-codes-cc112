752aea705f808dc9da718273ced05e4dd4e8422a
ninimsaJ <ivanclarklim2006@gmail.com>
2024-10-17 15:48:11 +0800

    program1.cpp

---
+++ images/program1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name;
+    cout << "Welcome To C++ Language Programming";
+    cout << "\nGood Morning Pal! What is your name?";
+    cin >> name;
+    cout << "Hi " + name +", Welcome to C++ Programming Class.\n";
+}
---
+++ images/program2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+int number1, number2, result;
+cout<<"Please enter your first number: ";
+cin>>number1;
+cout<<"Please enter your second number: ";
+cin>>number2;
+result = number1 + number2; le
+cout<<"Sum of the two numbers is "<<result;
+}
