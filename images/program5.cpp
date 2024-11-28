e35c23bd3900603c89f95b60d1d92d5dadd6ab8a
ninimsaJ <ivanclarklim2006@gmail.com>
2024-11-28 14:42:40 +0800

    program5.cpp

--- images/program5.cpp
+++ images/program5.cpp
@@ -9,7 +9,7 @@ cout<<"Please enter your exit time: ";
 cin>>exitTime;
 period = exitTime - entryTime;
 if (period > 1)
-charge (period) + 1;
+charge = (period) + 1;
 else
 charge = 2;
 cout<<"The charge for your parking is "<<charge;
