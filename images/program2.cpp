731fd1c072d7b53785e3af523c7e38a150a78dc5
ninimsaJ <ivanclarklim2006@gmail.com>
2024-11-28 14:32:21 +0800

    program2.cpp

--- images/program2.cpp
+++ images/program2.cpp
@@ -6,6 +6,6 @@ cout<<"Please enter your first number: ";
 cin>>number1;
 cout<<"Please enter your second number: ";
 cin>>number2;
-result = number1 + number2; le
+result = number1 + number2;
 cout<<"Sum of the two numbers is "<<result;
 }
